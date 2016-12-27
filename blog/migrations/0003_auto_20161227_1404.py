a2f2fe49c3095589abf8c9a37a46b9042aaa3b44
Your Name <you@example.com>
2016-12-27 14:31:23 +0200

    Ne

---
+++ blog/migrations/0003_auto_20161227_1404.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0002_post_approved_comment'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+                ('author', models.CharField(max_length=200)),
+                ('text', models.TextField()),
+                ('created_date', models.DateTimeField(default=django.utils.timezone.now)),
+                ('approved_comment', models.BooleanField(default=False)),
+            ],
+        ),
+        migrations.RemoveField(
+            model_name='post',
+            name='approved_comment',
+        ),
+        migrations.AddField(
+            model_name='comment',
+            name='post',
+            field=models.ForeignKey(related_name='comments', to='blog.Post'),
+        ),
+    ]
