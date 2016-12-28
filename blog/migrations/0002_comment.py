615c3854d1198a788872f60b4dd04a958702ee22
Your Name <you@example.com>
2016-12-28 14:41:34 +0200

    New

--- blog/migrations/0002_comment.py
+++ blog/migrations/0002_comment.py
@@ -8,27 +8,19 @@ import django.utils.timezone
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('blog', '0002_post_approved_comment'),
+        ('blog', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Comment',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
                 ('author', models.CharField(max_length=200)),
                 ('text', models.TextField()),
                 ('created_date', models.DateTimeField(default=django.utils.timezone.now)),
                 ('approved_comment', models.BooleanField(default=False)),
+                ('post', models.ForeignKey(to='blog.Post', related_name='comments')),
             ],
-        ),
-        migrations.RemoveField(
-            model_name='post',
-            name='approved_comment',
-        ),
-        migrations.AddField(
-            model_name='comment',
-            name='post',
-            field=models.ForeignKey(related_name='comments', to='blog.Post'),
         ),
     ]
